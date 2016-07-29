c38ffabe60c3c9961f7d2c615b86269d79e8de54
Michel Albert <michel@albert.lu>
2016-07-29 22:53:22 +0200

    Improved byte comparison tester.
    Can now compare sequences of differing length.

--- snmp/test/__init__.py
+++ snmp/test/__init__.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from os.path import dirname, join
 import unittest
 
@@ -11,18 +12,39 @@ class ByteTester(unittest.TestCase):
         """
         if a != b:
             comparisons = []
-            for offset, (char_a, char_b) in enumerate(zip(a, b)):
+            for offset, (char_a, char_b) in enumerate(zip_longest(a, b)):
                 comp, marker = ('==', '') if char_a == char_b else ('!=', '>>')
 
+                # Using "zip_longest", overflows are marked as "None", which is
+                # unambiguous in this case, but we need to handle these
+                # separately from the main format string.
+                if char_a is None:
+                    char_ab = char_ad = char_ah = '?'
+                else:
+                    char_ab = '0b{:08b}'.format(char_a)
+                    char_ad = '{:3d}'.format(char_a)
+                    char_ah = '0x{:02x}'.format(char_a)
+
+                if char_b is None:
+                    char_bb = char_bd = char_bh = '?'
+                else:
+                    char_bb = '0b{:08b}'.format(char_b)
+                    char_bd = '{:3d}'.format(char_b)
+                    char_bh = '0x{:02x}'.format(char_b)
+
                 comparisons.append(
-                    "{4:<3} Offset {0:4d}: "
-                    "0b{1:08b} {2} 0b{3:08b} | "
-                    "{1:3d} {2} {3:3d} | "
-                    "0x{1:02x} {2} 0x{3:02x}".format(
+                    "{8:<3} Offset {0:4d}: "
+                    "{1:^10} {4} {5:^10} | "
+                    "{2:>3} {4} {6:>3} | "
+                    "{3:^4} {4} {7:^4}".format(
                         offset,
-                        char_a,
+                        char_ab,
+                        char_ad,
+                        char_ah,
                         comp,
-                        char_b,
+                        char_bb,
+                        char_bd,
+                        char_bh,
                         marker))
             raise AssertionError('Bytes differ!\n' + '\n'.join(comparisons))
 
