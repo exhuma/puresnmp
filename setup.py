4ada98e3b998843a8c3afd2fca40eca7ea705cf2
Michel Albert <michel@albert.lu>
2016-08-03 11:09:02 +0200

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = open('snmp/version.txt').read().strip()
+
+setup(
+    name="puresnmp",
+    version=VERSION,
+    description="Pure Python SNMP implementation",
+    long_description=open("README.rst").read(),
+    author="Michel Albert",
+    author_email="michel@albert.lu",
+    license="BSD",
+    include_package_data=True,
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [],
+        'test': ['pytest-xdist', 'pytest']
+    },
+    packages=find_packages(exclude=["tests.*", "tests"]),
+)
