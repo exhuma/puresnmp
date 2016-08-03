7e46f20ce7fc6a5d76e73e0a5572923e0a9348fb
Michel Albert <michel@albert.lu>
2016-08-03 12:16:18 +0200

    Automatically determine network address family.

--- puresnmp/transport.py
+++ puresnmp/transport.py
@@ -5,11 +5,13 @@ This module mainly exist to enable a "seam" for mocking/patching out during
 testing.
 """
 import socket
+from ipaddress import ip_address
 
 
 def send(ip: str, port: int, packet: bytes) -> bytes:
-    sock = socket.socket(socket.AF_INET6,  # Internet
-                         socket.SOCK_DGRAM)  # UDP
+    checked_ip = ip_address(ip)
+    address_family = socket.AF_INET if checked_ip.version == 4 else socket.AF_INET6
+    sock = socket.socket(address_family, socket.SOCK_DGRAM)
 
     sock.sendto(packet, (ip, port))
     response = sock.recv(4096)
