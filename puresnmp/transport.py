6c2ac23c7364c7ffc8f7da403234491aa8c07590
Michel Albert <michel@albert.lu>
2016-08-26 08:17:17 +0200

    Removed "puresnmp.transport" from coverage reports.

--- puresnmp/transport.py
+++ puresnmp/transport.py
@@ -3,7 +3,12 @@ Low-Level network transport.
 
 This module mainly exist to enable a "seam" for mocking/patching out during
 testing.
+
+The module is excluded from coverage. It contains all the "dirty" stuff that's hard to test.
 """
+
+# TODO (beginner, no-dev): Ignore this file from coverage without adding "pragma: no cover" to each function.
+
 import socket
 import logging
 from ipaddress import ip_address
@@ -14,7 +19,7 @@ LOG = logging.getLogger(__name__)
 RETRIES = 3
 
 
-def send(ip: str, port: int, packet: bytes) -> bytes:
+def send(ip: str, port: int, packet: bytes) -> bytes:  # pragma: no cover
     checked_ip = ip_address(ip)
     address_family = socket.AF_INET if checked_ip.version == 4 else socket.AF_INET6
     sock = socket.socket(address_family, socket.SOCK_DGRAM)
@@ -33,6 +38,6 @@ def send(ip: str, port: int, packet: bytes) -> bytes:
     return response
 
 
-def get_request_id():
+def get_request_id():  # pragma: no cover
     from time import time
     return int(time())  # TODO check if this is good enough. My gut tells me "no"!
