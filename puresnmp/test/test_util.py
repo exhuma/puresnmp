4dad495071c824e274e7488c6899104b109e338e
Michel Albert <michel@albert.lu>
2016-11-03 17:38:14 +0100

    wip

---
+++ puresnmp/test/test_util.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from puresnmp.util import get_unfinished_walk_oids
+
+
+class TestUnfinishedOIDs(TestCase):
+
+    def test_single_continuing(self):
+        pass
+
+    def test_single_finishing(self):
+        pass
+
+    def test_single_uncontained(self):
+        pass
+
+    def test_multiple_continuing(self):
+        pass
+
+    def test_multiple_finishing(self):
+        pass
+
+    def test_multiple_uncontained(self):
+        pass
