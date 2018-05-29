32010337ab4ba9d51a2fe7306d921c3d52fcbb43
Michel Albert <michel@albert.lu>
2018-05-29 16:45:44 +0200

    Add link to RFC-3416

--- puresnmp/types.py
+++ puresnmp/types.py
@@ -1,9 +1,11 @@
 """
 SMI Types / Structure types which are not defined in :term:`X.690`.
 
-See `RFC 1155 section 3.2.3`_ for a description of the types.
+See `RFC 1155 section 3.2.3`_ for a description of the types and `RFC 3416`_
+for the definition of the new types.
 
 .. _RFC 1155 section 3.2.3: https://tools.ietf.org/html/rfc1155#section-3.2.3
+.. _RFC 3416: https://tools.ietf.org/html/rfc3416
 """
 
 # pylint: disable=missing-docstring
