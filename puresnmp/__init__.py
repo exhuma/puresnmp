fdfb6308fca7d8365b9be52219a537fb1fe4462b
Michel Albert <michel@albert.lu>
2018-05-29 16:43:28 +0200

    Ensure all types are properly loaded into Registry

--- puresnmp/__init__.py
+++ puresnmp/__init__.py
@@ -12,5 +12,10 @@ from puresnmp.api.pythonic import (bulkget, bulkwalk, get, getnext, multiget,
                                    multigetnext, multiset, multiwalk, set,
                                    table, walk)
 
+# !!! DO NOT REMOVE !!! The following import triggers the processing of SNMP
+# Types and thus populates the Registry. If this is not included, Non x.690
+# SNMP types will not be properly detected!
+import puresnmp.types
+
 __version__ = resource_string('puresnmp',
                               'version.txt').decode('ascii').strip()
