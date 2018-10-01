8d1e96380505211431ec0438ab7148a75788a761
Michel Albert <michel@albert.lu>
2018-10-01 14:33:50 +0200

    Add status-code to error message

--- puresnmp/exc.py
+++ puresnmp/exc.py
@@ -54,8 +54,8 @@ class ErrorResponse(SnmpError):
 
     def __init__(self, error_status, offending_oid, message=''):
         # type: (int, ObjectIdentifier, str) -> None
-        super().__init__('%s on OID %s' % (
-            message or self.DEFAULT_MESSAGE, offending_oid))
+        super().__init__('%s (status-code: %r) on OID %s' % (
+            message or self.DEFAULT_MESSAGE, error_status,  offending_oid))
         self.error_status = error_status
         self.offending_oid = offending_oid
 
