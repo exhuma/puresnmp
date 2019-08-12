7e4d06ab66c78d646c010c2281d19c339f5e56c0
Michel Albert <michel@albert.lu>
2019-08-12 07:43:14 +0200

    WIP

--- puresnmp/test/test_types.py
+++ puresnmp/test/test_types.py
@@ -7,6 +7,27 @@ from puresnmp import types as t
 
 
 @pytest.mark.parametrize('value, expected', [
+    (-2**31-1, ValueError),  # negative value below threshold
+    (-2**31, -2**31),  # negative value at threshold
+    (-42, 0),  # Negative number whithin bounds
+    (0, 0),  # Zero
+    (42, 42),  # positive number whithin bounds
+    (2**31, 2**31),  # positive value at threshold
+    (2**31-1, ValueError),  # positive value above threshold
+])
+def test_integer32(value, expected):
+    """
+    A counter instance should be a non-negative integer
+    """
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            t.Integer(value)
+    else:
+        instance = t.Integer(value)
+        assert instance.value == expected
+
+
+@pytest.mark.parametrize('value, expected', [
     (-42, 0),  # Underflow below threshold
     (-1, 0),  # Underflow at threshold
     (0, 0),  # The minimum value
