dfefa5ac195e104f9bd2be8b6e9bbda072fb040f
Michel Albert <michel@albert.lu>
2019-12-17 17:44:21 +0100

    Fix syntax for Python 8

--- puresnmp/api/pythonic.py
+++ puresnmp/api/pythonic.py
@@ -42,7 +42,7 @@ if TYPE_CHECKING:  # pragma: no cover
     T = TypeVar('T', bound=PyType)
 
 try:
-    unicode  # type: Callable[[Any], str]
+    unicode = unicode  # type: Callable[[Any], str]
 except NameError:
     # pylint: disable=invalid-name
     unicode = str  # type: Callable[[Any], str]
--- puresnmp/api/raw.py
+++ puresnmp/api/raw.py
@@ -45,7 +45,7 @@ if TYPE_CHECKING:  # pragma: no cover
     TFetcher = Callable[[str, str, List[str], int, int], List[VarBind]]
     T = TypeVar('T', bound=TType[PyType])  # pylint: disable=invalid-name
 try:
-    unicode  # type: Callable[[Any], str]
+    unicode = unicode  # type: Callable[[Any], str]
 except NameError:
     # pylint: disable=invalid-name
     unicode = str  # type: Callable[[Any], str]
