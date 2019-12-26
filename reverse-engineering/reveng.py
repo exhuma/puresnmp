ea9847a31eb4441415f1c11ddf57056f206fc375
Michel Albert <michel@albert.lu>
2019-12-26 13:21:41 +0100

    Add some files for reverse-engineering

--- reverse-engineering/reveng.py
+++ reverse-engineering/reveng.py
@@ -1,12 +1,12 @@
 from os.path import dirname
 
+HERE = dirname(__file__)
+
 from puresnmp.pdu import PDU
 from puresnmp.test import readbytes_multiple
 from puresnmp.x690.types import pop_tlv
 
-HERE = dirname(__file__)
-
-for row in readbytes_multiple('authpriv.hex', HERE):
+for row in readbytes_multiple("authpriv.hex", HERE):
     print(row)
     pdu, _ = pop_tlv(row)
     print(pdu.pretty())
