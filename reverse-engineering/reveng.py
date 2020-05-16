4d3c4e27222f1dd890a637710b379658a4f70493
Michel Albert <michel@albert.lu>
2020-05-16 08:37:06 +0200

    Fix the "readbytes" call

--- reverse-engineering/reveng.py
+++ reverse-engineering/reveng.py
@@ -1,8 +1,12 @@
-from puresnmp.test import readbytes_multiple
+from os.path import dirname
+
 from puresnmp.pdu import PDU
+from puresnmp.test import readbytes_multiple
 from puresnmp.x690.types import pop_tlv
 
-for row in readbytes_multiple('docker/authnopriv.hex'):
+HERE = dirname(__file__)
+
+for row in readbytes_multiple('authpriv.hex', HERE):
     print(row)
     pdu, _ = pop_tlv(row)
     print(pdu.pretty())
