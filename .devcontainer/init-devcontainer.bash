ea55ec238b8c314dd10766f782b36145c370eebf
Michel Albert <michel@albert.lu>
2024-07-25 15:47:33 +0200

    Fix dev-container setup

--- .devcontainer/init-devcontainer.bash
+++ .devcontainer/init-devcontainer.bash
@@ -3,5 +3,9 @@
 # This script is executed in the root-folder (as non-root user) of the project
 # when the container is created using the VS-Code Remote-Development extension.
 # -----------------------------------------------------------------------------
+pip install --user pipx
+pipx install fabric
+pipx install pre-commit
+pipx install docutils
 fab develop
 pre-commit install
